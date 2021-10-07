439cc0d0d3f8f6b8950bd8a6a1be84eecee74660
Varun Murlidhar <vmurlidhar@unc.edu>
2021-10-06 20:42:02 -0400

    lesson one progress

--- src/data/markdown/lesson_one.js
+++ src/data/markdown/lesson_one.js
@@ -1,11 +1,6 @@
 const lesson_one = `# Hi, I'm Varun!
 
-I am currently a freshman at UNC, and plan on 
-majoring in computer science. I was born in
-Chicago, but have been in Charlotte, NC, since
-the 4th grade. In my free time, I enjoy going on
-walks and reading mystery novels (especially 
-Agatha Christie!).
+I am currently a freshman at UNC, and plan on majoring in computer science. I was born in Chicago, but have been in Charlotte, NC, since the 4th grade. In my free time, I enjoy going on walks and reading mystery novels (especially Agatha Christie!).
 `;
 
 export default lesson_one;
